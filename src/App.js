e9f386a379326ad4f7be5a1989931f43ffba9e55
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-12 23:57:23 +0530

    redux code added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import appRouter from './routes/route';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebase/firebase';
 import { getAnalytics } from "firebase/analytics";
+import { useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from './utils/userSlice';
+
 
 function App() {
+  const dispatch = useDispatch()
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const auth = getAuth()
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }))
+      }
+      else {
+        dispatch(removeUser())
+      }
+    })
+
+  }, [])
   return (
     <>
       <RouterProvider router={appRouter}></RouterProvider>
