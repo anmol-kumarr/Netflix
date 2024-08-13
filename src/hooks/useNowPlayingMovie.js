59f0e0d5883e72392060cff01a869d0ab70b8350
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-14 01:43:41 +0530

    movi data fetch hooks added

---
+++ src/hooks/useNowPlayingMovie.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux"
+import { options } from "../constant"
+import { addNowPlayingMovie } from "../utils/movieSlice"
+import { useEffect } from "react"
+
+const useNowPlayingMovie = () => {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'
+    const dispatch = useDispatch()
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url, options)
+            const apiData = await response.json()
+            console.log(apiData)
+            // setData(apiData)
+            dispatch(addNowPlayingMovie(apiData))
+        }
+        catch (err) {
+            // setErr(true)
+            console.log(err)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    },[])
+}
+export default useNowPlayingMovie;
