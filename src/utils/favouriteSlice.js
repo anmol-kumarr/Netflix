d98c4b9ca8dee03c92f948594b8a0ad375d7d16d
Anmol kumar <anmolkumar31884@gmail.com>
2024-08-16 23:46:40 +0530

    favourite slice added

---
+++ src/utils/favouriteSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const FavouriteSlice=createSlice({
+    name:"favourite",
+    initialState:{
+        watchLater:[],
+        liked:[]
+    },
+    reducer:{
+        addWatchLater:(state,action)=>{
+            state.watchLater.push(action.payload)
+        },
+        removeWatchLater:(state,action)=>{
+            return state.watchLater.filter((item)=>item.id!==action.payload)
+        },
+        addLikedVideos:(state,action)=>{
+            state.liked.push(action.payload)
+        },
+        removeLiked:(state,action)=>{
+            state.liked.filter((item)=>item.id!==action.payload)
+        }
+    }
+})
+export const{addWatchLater,addLikedVideos,removeLiked,removeWatchLater}=FavouriteSlice.actions
+
+export default FavouriteSlice.reducer
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import movieSlice from "./movieSlice";
+import favouriteSlice from "./favouriteSlice";
 
 const store=configureStore({
     reducer:{
         user:userSlice,
-        movies:movieSlice
+        movies:movieSlice,
+        favourite:favouriteSlice
     }
 })
 export default store
